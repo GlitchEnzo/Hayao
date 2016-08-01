97fd6174dedfb450884bb8b576a6a8d94f9cfb98
GlitchEnzo <mccarthp@gmail.com>
2016-08-01 16:59:25 -0700

    Added log colors

--- Vapor/Core/Log.cs
+++ Vapor/Core/Log.cs
@@ -4,11 +4,19 @@
     {
         public static void Info(string message, params object[] args)
         {
+            System.Console.ForegroundColor = System.ConsoleColor.Gray;
+            System.Console.WriteLine(string.Format(message, args));
+        }
+
+        public static void Warning(string message, params object[] args)
+        {
+            System.Console.ForegroundColor = System.ConsoleColor.Yellow;
             System.Console.WriteLine(string.Format(message, args));
         }
 
         public static void Error(string message, params object[] args)
         {
+            System.Console.ForegroundColor = System.ConsoleColor.Red;
             System.Console.WriteLine(string.Format(message, args));
         }
     }
